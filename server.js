e9637a5a6d027ce23d92ab280011f43a0bb14801
mysersan <60083537+mysersan@users.noreply.github.com>
2020-04-07 14:22:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'app.js'));
+    res.sendFile(path.join(__dirname, '/src/app.js'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
